df8ce86e9d810d312fd5848da987ada4bb435d13
Marcley <marcleyrosa@gmail.com>
2024-03-07 18:32:03 -0300

    feat: clear state message with button

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
   const message = defineModel('message', { default: "Holle Word" })
+
+  function limpar() {
+    message.value = ''
+  }
 </script>
 <template>
   <h1>Meu primeiro Componente</h1>
 
   <div>
     Dizendo Olá: {{ message }}
+    <br>
+    <button @click="limpar">apaga tudo!</button>
   </div>
 
   <input v-model="message" />
