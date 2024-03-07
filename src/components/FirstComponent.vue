aa1f4cbcefbde0bd70bf1bb405f214aa252e3cfd
Marcley <marcleyrosa@gmail.com>
2024-03-07 17:54:40 -0300

    feat: config

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  const message = defineModel('message', { default: "Holle Word" })
+</script>
+<template>
+  <h1>Meu primeiro Componente</h1>
+
+  <div>
+    Dizendo Olá: {{ message }}
+  </div>
+
+  <input v-model="message" />
+</template>
