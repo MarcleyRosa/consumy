104c6e39a2015a73d1bd42f67501bf23f0f26e1a
Marcley <marcleyrosa@gmail.com>
2024-04-07 20:11:58 -0300

    feat: pagina inicial

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+
+</script>
+
+<template>
+  <main>
+    <h1>Delivety: Welcome</h1>
+    <nav>
+      <RouterLink :to="{ name: 'signin' }">Sign in</RouterLink>
+    </nav>
+  </main>
+</template>
