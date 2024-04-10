3fb36a925f378c88e237d299c644caeb44b45ba1
Marcley <marcleyrosa@gmail.com>
2024-04-10 02:50:43 -0300

    feat: funções de falha, sucesso, usuario atual e logout

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,19 @@
-export async function signIn(email: string, password: string, onSuccess: any) {
+import { storage } from "./storage";
+
+
+function success(response: Response, onSuccess: () => void) {
+  response.json().then((data) => {
+    storage.store('token', data.token)
+    storage.store('email', data.email)
+    onSuccess()
+  })
+}
+
+function failure(response: Response, onFailure: () => void) {
+  onFailure()
+}
+
+export async function signIn(email: string, password: string, onSuccess: () => void, onFailure: () => void) {
   console.log('will sign in...');
   const body = JSON.stringify({
     login: {
@@ -21,9 +36,43 @@ export async function signIn(email: string, password: string, onSuccess: any) {
 
   const response = await fetch(url, options)
 
-  response.json().then(() => onSuccess())
+  if (response.ok) {
+    success(response, onSuccess)
+  } else {
+    failure(response, onFailure)
+  }
+}
+
+function loggedIn() {
+  return !!storage.get('token')
+}
+
+function signOut(andThen: () => void) {
+  storage.remove('token')
+  storage.remove('email')
+
+  // if (typeof andThen === 'function') {
+  //   andThen()
+  // }
+
+  andThen()
+
+}
+
+function currentUser() {
+  if (!loggedIn()) {
+    return null
+  }
+
+  return {
+    email: storage.get('email')
+  }
+
 }
 
 export const auth = {
-  signIn
+  signIn,
+  loggedIn,
+  currentUser,
+  signOut
 }
