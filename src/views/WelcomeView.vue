2d4ca3ac90d51c34b3dc689b5fa388f2c8bdde03
Marcley <marcleyrosa@gmail.com>
2024-04-10 02:48:57 -0300

    feat: adicionando logout a pagina principal

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
+import { auth } from '@/auth';
+import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
 
+const currentUser = ref(auth.currentUser())
+
+const signOut = () => {
+  auth.signOut(() => {
+    currentUser.value = auth.currentUser()
+  })
+}
 
 </script>
 
 <template>
   <main>
     <h1>Delivety: Welcome</h1>
+    <template v-if="currentUser">
+      <h3>
+        Hi, {{ currentUser.email }}
+      </h3>
+      <br>
+      <nav>
+        <a @click="signOut"></a>
+      </nav>
+    </template>
     <nav>
       <RouterLink :to="{ name: 'signin' }">Sign in</RouterLink>
     </nav>
