96ad6e5a106fab2599b703299c26c38dc744a042
Marcley <marcleyrosa@gmail.com>
2024-05-30 21:57:51 -0300

    fix: removendo class

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+
 defineProps<{
   msg: string
 }>()
 </script>
 
 <template>
-  <div class="greetings cent">
+  <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
   </div>
 </template>
@@ -16,17 +17,6 @@ h1 {
   font-size: 2.6rem;
   position: relative;
   top: -10px;
-}
-
-.cent {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  background-color: #f4f4f4;
-  text-align: center;
-  padding: 10px;
-  box-shadow: 0 2px 5px rgba(0,0,0,0.1);
 }
 
 h3 {
