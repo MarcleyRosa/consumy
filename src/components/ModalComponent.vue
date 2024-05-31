10e70ea706862f8129425afb7e550a5f55999438
Marcley <marcleyrosa@gmail.com>
2024-05-30 21:53:29 -0300

    feat: iniciando modal para componente de adicionar produto

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div v-if="isVisible" class="modal-overlay" @click.self="close">
+    <div class="modal-content">
+      <input type="text">
+      <button class="close-button" @click="close">X</button>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const isVisible = ref(true)
+
+const close = () => {
+  isVisible.value = false
+}
+
+</script>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  position: relative;
+}
+
+.close-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: transparent;
+  border: none;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
+
