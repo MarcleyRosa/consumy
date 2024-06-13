dec7352eb2070a9bcc7663bf3218386fd3f3e82f
Marcley <marcleyrosa@gmail.com>
2024-06-13 15:36:22 -0300

    fix: nome da env

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -15,7 +15,7 @@ class Request {
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        "X-API-KEY": import.meta.env.X_API_KEY,
+        "X-API-KEY": import.meta.env.VITE_X_API_KEY,
         "Authorization": `Bearer ${token}`
       },
     };
