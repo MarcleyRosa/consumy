f7da1dd5981d2ded7babd19851893febe52c5f35
Marcley <marcleyrosa@gmail.com>
2024-06-13 07:18:45 -0300

    feat: examplo de env e import de env X-API-KEY no fetch

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -15,7 +15,7 @@ class Request {
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        "X-API-KEY": "D+PQFRWy9hb/RqN2bYZnHXUNIMY=",
+        "X-API-KEY": import.meta.env.X_API_KEY,
         "Authorization": `Bearer ${token}`
       },
     };
@@ -57,6 +57,41 @@ class Request {
 
   public delete<T>(endpoint: string): Promise<T> {
     return this.request<T>(endpoint, 'DELETE');
+  }
+  public async uploadImage<T>(endpoint: string, body: any): Promise<T> {
+      const url = `${this.baseURL}${endpoint}`;
+      const formData = new FormData();
+
+      // const convertBody = Object.entries(body)
+
+      // convertBody.forEach((data) => {
+      //   const [key, value] = data;
+      //   formData.append(key, value as any)
+      // })
+      formData.append('store[name]', body.name);
+      formData.append('store[image]', body.image);
+      
+
+      const options = {
+        method: 'POST',
+          body: formData,
+          headers: {
+            // 'Content-Type': 'multipart/form-data' 
+          },
+      }
+      try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        if (response.status === 204) {
+          return null as T;
+        }
+        return await response.json();
+      } catch (error) {
+        console.error('Fetch error:', error);
+        throw error;
+      }
   }
 }
 
