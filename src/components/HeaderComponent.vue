7b0517b4c6112cb6e522b5ee282ab610b695b256
Marcley <marcleyrosa@gmail.com>
2024-06-19 22:57:57 -0300

    feat: estilização e adicionando icons

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,11 +3,25 @@ import { ref } from 'vue';
 import ModalCart from './ModalCart.vue';
 import UserProfile from './UserProfile.vue';
 import { useRoute, useRouter } from 'vue-router';
+import { PhShoppingCart, PhArrowArcLeft, PhSignOut, PhCalendarDots } from "@phosphor-icons/vue";
+import { Auth } from '@/utils/auth';
 
 
 const route = useRoute()
 const router = useRouter()
 const isModal = ref(false)
+
+const auth = new Auth()
+
+const isLoggedIn = ref(auth.isLoggedIn())
+
+const signOut = () => {
+  auth.signOut(() => {
+    isLoggedIn.value = auth.isLoggedIn()
+  })
+  router.push('/')
+}
+
 
 
 </script>
@@ -24,24 +38,23 @@ const isModal = ref(false)
         <button
           v-if="route.path !== '/orders'"
           @click="() => router.push('/orders')"
-          class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
         >
-          Meus pedidos
+        <PhCalendarDots :size="32" weight="fill" />
+        </button>
+        <button @click="router.back()">
+          <PhArrowArcLeft :size="32" weight="fill" />
         </button>
         <button
           @click="() => isModal = !isModal"
-          class="flex items-center bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M14 4a2 2 0 0 1 2 2v.153l-1.061 6.365a2 2 0 0 1-1.973 1.634H7.034a2 2 0 0 1-1.973-1.634L4 6.153V6a2 2 0 0 1 2-2h8ZM5.293 7.293A1 1 0 0 1 6 7h8a1 1 0 0 1 .707 1.707L13.414 14H6.586l-1.293-5.293ZM7 16a2 2 0 1 1 4 0 2 2 0 0 1-4 0Z"/>
-          </svg>
-          Carrinho
+        <PhShoppingCart :size="32" weight="fill" />
         </button>
-        <button @click="() => router.push('/stores')" class="flex items-center bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M10 18a1 1 0 0 1-1-1v-3.586c0-.377-.21-.725-.553-.895l-4-2.5a1 1 0 0 1 0-1.61l4-2.5a1 1 0 0 1 1.106.104A1 1 0 0 1 11 7.586V11a1 1 0 0 0 1 1h4a1 1 0 0 1 0 2h-4v2a1 1 0 0 1-1 1Zm1-12.414V3a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v2.414L3.276 8.07 6 9.828V13a1 1 0 0 0 1 1h4a1 1 0 0 1 0 2H7a3 3 0 0 1-3-3v-3.172l1.724-1.758L7 4.414V3a3 3 0 0 1 3-3h6a3 3 0 0 1 3 3v1.414l1.276 1.758L14 8.828V12a1 1 0 0 1-1 1h-4a1 1 0 0 0-1 1v2h4a3 3 0 0 0 3-3V9.828l2.724-1.758L17 5.414V3a3 3 0 0 0-3-3H6a3 3 0 0 0-3 3v2.414l2.724 1.758L10 7.828V11a1 1 0 0 0 1 1h4a1 1 0 0 1 0 2h-4v-2Z"/>
-          </svg>
-        </button>
+        <button
+            @click="signOut"
+  
+          >
+          <PhSignOut :size="32" weight="fill" />
+          </button>
       </div>
     </div>
   </div>
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,21 +1,11 @@
 <script setup lang="ts">
 import { Auth } from '../utils/auth';
 import { ref } from 'vue';
-import { RouterLink, useRouter } from 'vue-router';
+import { RouterLink } from 'vue-router';
 
 const auth = new Auth()
-const router = useRouter()
 
-const isLoggedIn = ref(auth.isLoggedIn())
 const currentUser = ref(auth.currentUser())
-
-const signOut = () => {
-  auth.signOut(() => {
-    isLoggedIn.value = auth.isLoggedIn()
-  })
-  router.push('/')
-}
-
 
 </script>
 
@@ -27,15 +17,6 @@ const signOut = () => {
           <p class="text-sm lg:text-base font-semibold text-gray-800 mb-2 lg:mb-0">
             Hi, {{ currentUser && currentUser.email }}
           </p>
-          <button
-            @click="signOut"
-            class="bg-blue-500 text-white px-4 py-2 rounded-md flex items-center space-x-2 mt-2 lg:mt-0 lg:ml-4 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3m10 8V4m0 16v-8"></path>
-            </svg>
-            Sair
-          </button>
         </div>
       </template>
       <template v-else>
