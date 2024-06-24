16acafd921939276a9925d0bbaa8042d7a3a33a4
Marcley <marcleyrosa@gmail.com>
2024-06-24 02:22:31 -0300

    refactory: componentização de product e store

--- src/views/StoresView.vue
+++ src/views/StoresView.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import { Request } from '../utils/fetch';
-import { useRouter } from 'vue-router';
-import imageStore from '../assets/loja-sem-foto.png';
 import noProductImg from '../assets/produto-sem-foto.png';
+import StoreComponenent from '../components/StoreComponent.vue'
 
 const data = ref([]);
-const router = useRouter();
 const products = ref({ result: { products: [] } });
-
-const start = (id: string) => {
-  router.push(`/store/${id}/products`);
-};
 
 const carouselWrapper = ref<HTMLElement | null>(null);
 const currentIndex = ref(0);
@@ -83,39 +77,7 @@ onMounted(async () => {
       </button>
     </div>
     <h2 class="text-2xl font-bold mb-4">Lojas:</h2>
-    <div class="flex flex-wrap -mx-4">
-      <div
-        v-for="{ name, id, image_url, active } in data"
-        :key="id"
-        :id="id"
-        class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mb-4"
-      >
-        <button
-          :disabled="!active"
-          @click="() => start(id)"
-          class="flex items-center bg-white rounded-lg p-4 w-full h-full transition-transform duration-300 ease-in-out transform hover:shadow-md hover:scale-105"
-          :class="{ 'opacity-50 cursor-not-allowed': !active }"
-        >
-          <img
-            v-if="image_url"
-            :src="url + image_url"
-            alt="Store Image"
-            class="w-16 h-16 rounded-full"
-          />
-          <img
-            v-else
-            :src="imageStore"
-            alt="Default Image"
-            class="w-16 h-16 rounded-full"
-          />
-          <div class="ml-4">
-            <span class="text-lg font-semibold block">{{ name }}</span>
-            <span v-if="!active" class="block text-sm text-red-500">Fechado</span>
-            <span v-else class="block text-sm text-gray-500">Loja</span>
-          </div>
-        </button>
-      </div>
-    </div>
+    <StoreComponenent :data="data" />
   </div>
 </template>
 
